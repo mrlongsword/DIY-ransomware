f0a6c3622501dd89a4ec10a1037d27b02ba74e87
mrlongsword <60816759+mrlongsword@users.noreply.github.com>
2021-07-12 19:35:50 +0800

    Update decryptor.py

--- decryptor.py
+++ decryptor.py
@@ -2,19 +2,20 @@ import pyAesCrypt
 import os
 import sys
 cwd = os.getcwd()
-#print(cwd)
-key = "password"
+
+key = input("Enter the key:")
 for root,dirs,files in os.walk(cwd):
     for file in files:
-        #print(root+"\\"+file)
+        
         encrypted = (root+"\\"+file)
         file_path,file_ext = os.path.splitext(root+"\\"+file)
         decrypted = file_path
-        #print(file_ext)
-        if file_ext == ".foobar":
-            #print(encrypted)
-            pyAesCrypt.decryptFile(encrypted, decrypted, key)
-            os.remove(encrypted)
         
-                
-#print(sys.argv[0])
+        if file_ext == ".foobar":
+            try:
+                print("Attempting to decrypt",str(encrypted))
+                pyAesCrypt.decryptFile(encrypted, decrypted, key)
+                print("Decrypting",str(encrypted))
+                os.remove(encrypted)
+            except:
+                print("Error!Invalid key.")
